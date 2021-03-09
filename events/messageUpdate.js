98ed6e4d5d029ff8059833c1c9b62dfaedd426c8
ValAlpha <lukeanthonysmith96@gmail.com>
2021-03-09 13:35:49 +0000

    Imagine being dumb like me and ruining a repo.

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-const { MessageEmbed } = require("discord.js")
-
-const messageDelete = (client => {
-  client.on('messageUpdate', async (msg) => {
-
-    if(msg.author.bot) return
-
-    const settings = client.settings
-    if(!settings.messageLogs) return
-
-    let oldContent = msg.content
-    let newContent = msg.reactions.message.content
-
-    if(oldContent === newContent) return
-
-    client.channels.cache.get(settings.messageLogs).send(new MessageEmbed()
-    .setAuthor(client.user.username, client.user.displayAvatarURL({dynamic: true}))
-    .setTitle(`Message Edited`)
-    .setDescription(`
-    A message by <@${msg.author.id}> was edited
-    
-    • Before: \`\`\`${oldContent}\`\`\`
-    
-    • After: \`\`\`${newContent}\`\`\``)
-    .setColor("RANDOM")
-    .setTimestamp()).catch(err => console.log(err))
-  })
-})
-
-=======
 const { MessageEmbed } = require("discord.js")
 
 const messageDelete = (client => {
