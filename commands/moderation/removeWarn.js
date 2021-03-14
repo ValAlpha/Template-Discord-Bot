7a39e9141f76df45c4083b674dd08026fa99b472
ValAlpha <lukeanthonysmith96@gmail.com>
2021-03-14 21:40:52 +0000

    Remove warning by case ID

---
+++ commands/moderation/removeWarn.js
@@ -0,0 +1,42 @@
+const { Command } = require("discord.js-commando")
+const { MessageEmbed } = require("discord.js")
+
+module.exports = class removewarn extends Command { 
+  constructor(client){
+    super(client, {
+      name: "removewarn", 
+      description: "Remove a warn by it's case ID", 
+      memberName: "removewarn", 
+      group: "moderation",
+      guildOnly: true, 
+      aliases: ["rw"], 
+      args: [{
+        type: "integer", 
+        prompt: "Which case number?", 
+        key: "caseID", 
+        min: 1
+      }]
+    })
+  }
+
+  async run(msg, { caseID }) {
+
+    const warns = await this.client.dbs.warnings.findOne({dbID: msg.guild.id})
+
+    if(!warns) return msg.say(`There are no warnings for this server`)
+
+    const findWarning = warns.warnings.find(w => w.caseID === caseID)
+    if(!findWarning) return msg.say(`There is no warning with that case ID`)
+
+    warns.warnings = warns.warnings.filter(w => w.caseID !== caseID)
+
+    warns.save().catch(err => console.log(err))
+
+    return msg.say(`Warning removed`)
+    
+
+    
+
+  }
+
+}
