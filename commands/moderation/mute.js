5e3f182539bfa103b8adb9b813b87d720f96b592
ValAlpha <lukeanthonysmith96@gmail.com>
2021-03-19 16:28:13 +0000

    Format fix

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -1,4 +1,4 @@
-                      const { Command } = require("discord.js-commando")
+const { Command } = require("discord.js-commando")
 const { MessageEmbed } = require("discord.js")
 
 module.exports = class mute extends Command {
